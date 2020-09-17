35e718e97d86fd2a792c41d03efc4bd8d10558c0
Steven Jin <jinrq@google.com>
2020-09-17 03:49:07 -0400

    read env

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var app = express();
+const ip = process.env.PRODUCTPAGE_SERVICE_HOST;
+const port = process.env.PRODUCTPAGE_SERVICE_PORT;
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World! production page: ' + ip + ':' + port);
 });
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
